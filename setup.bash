d8d69fac3b7946063431ea50605b13bd0718ad41
odinblister <44077144+odinblister@users.noreply.github.com>
2019-02-08 20:23:37 -0700

    Added systemd configuration for auto-restart
    This version of the script adds an odin.system service file in /etc/systemd/system that will allow the odind deamon to be managed by the systemd process manager.  Now, odind should automatically restart itself if the VPS has to reboot.

--- setup.bash
+++ setup.bash
@@ -44,8 +44,45 @@ mnkey=$(odin-cli masternode genkey)
 echo "masternode=1" >> $config
 echo "masternodeprivkey=$mnkey" >> $config
 odin-cli stop
-odind
 
+#Set up odin masternode systemd service file
+echo "Setting up Odin masternode system service"
+servicefile=/etc/systemd/system/odin.service
+if [ -f $servicefile ];
+then
+    cp $servicefile $servicefile.bak
+    rm $servicefile
+fi
+echo "[Unit]" >> $servicefile
+echo "Description=Odin Blockchain Daemon" >> $servicefile
+echo "After=network.target" >> $servicefile
+echo " " >> $servicefile
+echo "[Service]" >> $servicefile
+echo "User=root" >> $servicefile
+echo "Group=root" >> $servicefile
+echo "Type=forking" >> $servicefile
+echo "PIDFile=/root/.odin/odin.pid" >> $servicefile
+echo "ExecStart=/usr/bin/odind -daemon -pid=/root/.odin/odin.pid -conf=/root/.odin/odin.conf -datadir=/root/.odin" >> $servicefile
+echo "Restart=always" >> $servicefile
+echo "RestartSec=5" >> $servicefile
+echo "PrivateTmp=true" >> $servicefile
+echo "TimeoutStopSec=60s" >> $servicefile
+echo "TimeoutStartSec=5s" >> $servicefile
+echo "StartLimitInterval=120s" >> $servicefile
+echo "StartLimitBurst=15" >> $servicefile
+echo " " >> $servicefile
+echo "[Install]" >> $servicefile
+echo "WantedBy=multi-user.target" >> $servicefile
+
+# Enable masternode service so it starts at boot, and start masternode
+odin_enable="systemctl enable odin"
+odin_start="systemctl start odin"
+echo "Enabling odin masternode systemd service..."
+eval $odin_enable
+echo "Starting odin masternode..."
+eval $odin_start
+
+#finishing up 
 echo " "
 echo "Masternode VPS setup complete."
 echo "(it will now take a few minutes for the mn to get ready)"
